2ccbf1aba58037bce2f2f983c6fc7dc9dc8d84cc
Daisy Smith <daisymarie128@gmail.com>
2017-04-13 15:09:47 +0100

    autoplay logic

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import Carousel from './modules/carousel/carousel';
 
-const test = new Carousel({
-    images: [
-        'http://www.fillmurray.com/g/500/300',
-        'http://www.fillmurray.com/g/600/300',
-        'http://www.fillmurray.com/g/800/300'
-    ]
-});
+const images = [
+  'http://www.fillmurray.com/g/500/300',
+  'http://www.fillmurray.com/g/600/300',
+  'http://www.fillmurray.com/g/800/300'
+]
 
-test.init()
+const myCarousel = new Carousel(500, images);
+
+myCarousel.init()
 
